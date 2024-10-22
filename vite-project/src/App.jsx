f0858105b2c5ebd9c706422fa90b4c1f05c123f2
pradhanmausumi <pradhanmausumi68@gmail.com>
2024-10-22 07:54:01 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Pokedex from "./compnents/Pokedex/Pokedex"
+
+
+function App() {
+
+  return (
+    <>
+     <Pokedex/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/compnents/PokemonList/PokemonList.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import './PokemonList.css';
+import Pokemon from "../pokemon/pokemon";
+
+function PokemonList() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+const [pokedexUrl,setPokedexUrl]=useState('https://pokeapi.co/api/v2/pokemon');
+const[nextUrl,setNextUrl]=useState('');
+const[prevUrl,setPrevUrl]=useState('');
+
+  async function downloadPokemons() {
+    setIsLoading(true)
+    const response = await axios.get(pokedexUrl);//this downloads list of pokemon
+    const pokemonResults = response.data.results;//we get the array of pokemon from result
+    console.log(response.data)
+    setNextUrl(response.data.next)
+    setPrevUrl(response.data.previous)
+    //iterating over the array of pokemons and using their url to create an array of promise
+    //that will download 20 pokemons
+    const pokemonPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
+//passing promise array  to axios  all
+    const pokemonData = await axios.all(pokemonPromise);//array of pokemon detailed data
+   console.log(pokemonData)
+
+//now iterate data of each pokemon  extract their id name image
+    const res = pokemonData.map((pokeData) => {
+      const pokemon = pokeData.data;
+
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: (pokemon.sprites.other && pokemon.sprites.other.dream_world)
+          ? pokemon.sprites.other.dream_world.front_default
+          : pokemon.sprites.front_shiny,
+        types: pokemon.types
+      };
+    });
+
+    setPokemonList(res);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    downloadPokemons();
+  }, [pokedexUrl]);
+
+  return (
+    <>
+      <div className="pokemon-list-wrapper">
+       <div className="pokemon-wrapper">  {isLoading ? "Loading..." :
+          pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} />)}
+          </div>
+         <div className="controls">
+           <button disabled={prevUrl==null} onClick={() => setPokedexUrl(prevUrl)} >Prev</button>
+          <button disabled={nextUrl==null} onClick={() => setPokedexUrl(nextUrl)}>Next</button>
+         </div>
+      </div>
+    </>
+  );
+}
+
+export default PokemonList;
